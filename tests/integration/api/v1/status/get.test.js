fad30007434bcabc1a24b9095c5f3046edc70b19
thipperz <142650266+thipperz@users.noreply.github.com>
2023-12-15 03:41:00 +0000

    Adiciona testes de database`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
+  /* expect(responseBody.used_connections).toBeDefined();
+  expect(typeof responseBody.used_connections).toBe("number");*/
 });
